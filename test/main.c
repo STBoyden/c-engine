a1269a95b1f8985e1422373bccff1dbdcbcdae97
STBoyden <sam@stboyden.com>
2021-03-28 22:09:58 +0100

    Add Window tests

--- test/main.c
+++ test/main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include "rect.h"
 #include "types.h"
+#include "window.h"
 
-bool test_rectangle_intersect();
+bool test_rectangle_intersect	();
+bool test_window_creation		();
 
 int main() {
     str result = test_rectangle_intersect() ? "passed" : "failed";
     printf("test_rectangle_intersect (expected pass) got: %s\n", result);
+
+    result = test_window_creation() ? "passed" : "failed";
+    printf("test_window_creation (expected pass) got: %s\n", result);
 }
 
 
@@ -15,4 +20,24 @@ bool test_rectangle_intersect() {
     Rect r2 = Rect_create(100, 200, Point2d_create(50, 100));
 
     return Rect_intersects(&r1, &r2);
+}
+
+bool test_window_creation() {
+    Window win = Window_create("Test Window", 1280, 720);
+
+    SDL_Event event;
+
+    int width = 0;
+    int height = 0;
+
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            default:
+                SDL_GetWindowSize(win.win_ptr, &width, &height);
+                Window_destroy(&win);
+                break;
+        }
+    }
+
+    return (width == win.dimensions.width) && (height == win.dimensions.height);
 }
--- test/test.pro
+++ test/test.pro
@@ -2,13 +2,26 @@ TEMPLATE = app
 CONFIG += console
 CONFIG -= app_bundle
 CONFIG -= qt
+QMAKE_CFLAGS += -std=c99
 
 SOURCES += \
         main.c
 
-win32:CONFIG(release, debug|release): LIBS += -L$$OUT_PWD/../core/release/ -lcore
-else:win32:CONFIG(debug, debug|release): LIBS += -L$$OUT_PWD/../core/debug/ -lcore
-else:unix: LIBS += -L$$OUT_PWD/../core/ -lcore
+INCLUDEPATH += $$PWD/../windowing
+DEPENDPATH += $$PWD/../windowing
+
+unix|win32: LIBS += -lSDL2
+
+win32:CONFIG(release, debug|release): LIBS += -L$$OUT_PWD/../core/release/ -lengine-core
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$OUT_PWD/../core/debug/ -lengine-core
+else:unix: LIBS += -L$$OUT_PWD/../core/ -lengine-core
 
 INCLUDEPATH += $$PWD/../core
 DEPENDPATH += $$PWD/../core
+
+win32:CONFIG(release, debug|release): LIBS += -L$$OUT_PWD/../windowing/release/ -lengine-windowing
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$OUT_PWD/../windowing/debug/ -lengine-windowing
+else:unix: LIBS += -L$$OUT_PWD/../windowing/ -lengine-windowing
+
+INCLUDEPATH += $$PWD/../windowing
+DEPENDPATH += $$PWD/../windowing
