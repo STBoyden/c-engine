c44217fa0a4ea645ab06edd7642aa1b05bcd819d
STBoyden <sam@stboyden.com>
2021-03-28 18:37:26 +0100

    Add tests for core

---
+++ test/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "rect.h"
+#include "types.h"
+
+bool test_rectangle_intersect();
+
+int main() {
+    str result = test_rectangle_intersect() ? "passed" : "failed";
+    printf("test_rectangle_intersect (expected pass) got: %s\n", result);
+}
+
+
+bool test_rectangle_intersect() {
+    Rect r1 = Rect_create(100, 200, Vector2d_create(0, 0));
+    Rect r2 = Rect_create(100, 200, Vector2d_create(50, 100));
+
+    return Rect_intersects(&r1, &r2);
+}
---
+++ test/test.pro
@@ -0,0 +1,14 @@
+TEMPLATE = app
+CONFIG += console
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+        main.c
+
+win32:CONFIG(release, debug|release): LIBS += -L$$OUT_PWD/../core/release/ -lcore
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$OUT_PWD/../core/debug/ -lcore
+else:unix: LIBS += -L$$OUT_PWD/../core/ -lcore
+
+INCLUDEPATH += $$PWD/../core
+DEPENDPATH += $$PWD/../core
