c98a7e1abdc1b3ca246904711efb9cb039c02bd1
STBoyden <sam@stboyden.com>
2021-03-28 22:05:32 +0100

    Added comments to Rect struct

--- core/rect.h
+++ core/rect.h
@@ -11,6 +11,7 @@ struct Bounds {
     Point2d bottom_right;
 };
 
+/// Rect struct: useful for sprites and windows
 typedef struct {
     i32 			width;
     i32 			height;
@@ -18,8 +19,11 @@ typedef struct {
     struct Bounds 	bounds;
 } Rect;
 
+/// Creates a Rect
 extern Rect	Rect_create		(i32, i32, Point2d);
+/// Initialises a Rect pointer
 extern void	Rect_init		(Rect*, i32, i32, Point2d);
+/// Checks if two Rects intersect
 extern bool	Rect_intersects	(const Rect*, const Rect*);
 
 #endif // RECT_H
