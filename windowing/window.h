de83add77594d358fa26787dbbcd130a116852ab
STBoyden <sam@stboyden.com>
2021-03-28 18:38:55 +0100

    Initial commit for windowing project

---
+++ windowing/window.h
@@ -0,0 +1,12 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <SDL2/SDL.h>
+
+typedef struct {
+    SDL_Window* 	window;
+} Window;
+
+//Window	window_create	();
+
+#endif // WINDOW_H
---
+++ windowing/windowing.pro
@@ -0,0 +1,11 @@
+TEMPLATE = lib
+CONFIG += console
+CONFIG -= app_bundle
+CONFIG -= qt
+#INCLUDEPATH += "../core"
+
+SOURCES += \
+    window.c
+
+HEADERS += \
+    window.h
