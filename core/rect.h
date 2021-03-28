c8c5ed17876cf511b1a525c80ced02832a7b9ae9
STBoyden <sam@stboyden.com>
2021-03-28 22:22:02 +0100

    Re-indent rect.h attempt 2

--- core/rect.h
+++ core/rect.h
@@ -13,16 +13,16 @@ struct Bounds {
 
 /// Rect struct: useful for sprites and windows
 typedef struct {
-    i32 			width;
-    i32 			height;
+    i32				width;
+    i32				height;
     Point2d 		position;
     struct Bounds 	bounds;
 } Rect;
 
 /// Creates a Rect
-extern Rect	Rect_create 	(i32, i32, Point2d);
+extern Rect	Rect_create		(i32, i32, Point2d);
 /// Initialises a Rect pointer
-extern void	Rect_init 		(Rect*, i32, i32, Point2d);
+extern void	Rect_init		(Rect*, i32, i32, Point2d);
 /// Checks if two Rects intersect
 extern bool	Rect_intersects	(const Rect*, const Rect*);
 
